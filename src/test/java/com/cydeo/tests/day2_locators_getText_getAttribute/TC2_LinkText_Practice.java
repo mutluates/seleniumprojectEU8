7245dd7202aa2643fdc574bfec2af1ce3b22195e
makbas06 <mutluates135@gmail.com>
2022-05-05 12:08:19 +0300

    tc2 initiate

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/TC2_LinkText_Practice.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class TC2_LinkText_Practice {
+
+    public static void main(String[] args) {
+
+       // TC #2: Back and forth navigation
+       // 1- Open a Chrome browser
+       // 2- Go to: https://practice.cydeo.com
+       // 3- Click to A/B Testing from top of the list.
+       // 4- Verify title is:
+       // Expected: No A/B Test
+       // 5- Go back to home page by using the .back();
+       // 6- Verify title equals:
+       // Expected: Practice
+
+
+    }
+}
