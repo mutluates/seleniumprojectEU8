f101b521a6d094f40a79e10492e87095da088fa4
makbas06 <mutluates135@gmail.com>
2022-05-05 16:32:24 +0300

    tc4 finished

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/TC4_LibraryLoginPage.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/TC4_LibraryLoginPage.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC4_LibraryLoginPage {
@@ -14,13 +16,21 @@ public class TC4_LibraryLoginPage {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
-        // 2. Go to http://library2.cybertekschool.com/login.html
-        driver.navigate().to("http://library2.cybertekschool.com/login.html");
+        // 2. Go to https://library2.cybertekschool.com/login.html
+        driver.navigate().to("https://library2.cybertekschool.com/login.html");
 
         // 3. Enter username: “incorrect@email.com”
+        WebElement usernameInput = driver.findElement(By.className("form-control"));
+        usernameInput.sendKeys("incorrect@email.com");
 
         // 4. Enter password: “incorrect password”
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
+
         // 5. Click to Sign in button
+        WebElement signButton = driver.findElement(By.tagName("button"));
+        signButton.click();
+
         // 6. Verify: visually “Sorry, Wrong Email or Password” displayed
 
 
