a4ded54abb2aa120fb153b4bc5d77bf6e412ae90
makbas06 <mutluates135@gmail.com>
2022-05-05 10:30:54 +0300

    tc1 created

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/TC1_CydeoVerifications.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class TC1_CydeoVerifications {
+
+   // TC #1: Cydeo practice tool verifications
+   // 1. Open Chrome browser
+   // 2. Go to https://practice.cydeo.co
+   // 3. Verify URL contains
+   // Expected: Cydeo
+   // 4. Verify title:
+   //  Expected: Practice
+}
