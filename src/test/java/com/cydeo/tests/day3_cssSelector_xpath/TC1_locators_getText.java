5c8a74f813f368f7a30d7e054dba17367109dfa5
makbas06 <mutluates135@gmail.com>
2022-05-06 12:05:27 +0300

    utilities done

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/TC1_locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/TC1_locators_getText.java
@@ -1,10 +1,9 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC1_locators_getText {
 
@@ -12,8 +11,9 @@ public class TC1_locators_getText {
 
         //  TC #1: NextBaseCRM, locators and getText() practice
         //   1- Open a Chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+      //  WebDriverManager.chromedriver().setup();
+      //  WebDriver driver = new ChromeDriver();
+        WebDriver driver = WebDriverFactory.getDriver("edge");
         driver.manage().window().maximize();
 
         //   2- Go to: https://login1.nextbasecrm.com/
