43c63740dd2cad0c0108c7f5990eff623d2dd7b8
makbas06 <mutluates135@gmail.com>
2022-05-12 12:00:41 +0300

    tc3 utility created

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TC3_RadioButton_cont.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TC3_RadioButton_cont.java
@@ -22,24 +22,29 @@ public class TC3_RadioButton_cont {
         driver.get("https://practice.cydeo.com/radio_buttons");
 
         // 3. Locate name='sports' radio buttons and store them in a list of Web Element
-        List<WebElement> sportRadioButtons = driver.findElements(By.name("sport"));
+
+        clickAndVerifyRadioButton(driver, "sport", "hockey");
+
+
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue) {
+        List<WebElement> RadioButtons = driver.findElements(By.name(nameAttribute));
 
         // 4. Loop through the list of WebElement and select matching result "hockey"
-        for (WebElement each : sportRadioButtons) {
+        for (WebElement each : RadioButtons) {
 
             String eachId = each.getAttribute("id");
 
             System.out.println("eachId = " + eachId);
 
-            if (eachId.equals("hockey")){
+            if (eachId.equals("hockey")) {
                 each.click();
-                System.out.println("Hockey is selected : " + each.isSelected());
+                System.out.println(eachId + " is selected : " + each.isSelected());
                 break;
             }
+
         }
 
-
-
     }
-
 }
