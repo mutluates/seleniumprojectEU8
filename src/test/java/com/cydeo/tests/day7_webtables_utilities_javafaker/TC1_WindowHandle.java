aa9e43edb9106f22fc974f0eaf7da90c9270ea8c
makbas06 <mutluates135@gmail.com>
2022-05-19 15:27:43 +0300

    new utility updated

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/TC1_WindowHandle.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/TC1_WindowHandle.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -12,21 +13,21 @@ import java.util.concurrent.TimeUnit;
 
 public class TC1_WindowHandle {
 
-   public WebDriver driver;
+    public WebDriver driver;
 
-   @BeforeMethod
-   public void setupMethod(){
+    @BeforeMethod
+    public void setupMethod() {
 
-       //TC #1: Window Handle practice
-       // 1. Create new test and make set ups
-       driver = WebDriverFactory.getDriver("chrome");
-       driver.manage().window().maximize();
-       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //TC #1: Window Handle practice
+        // 1. Create new test and make set ups
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-   }
+    }
 
     @Test
-    public void window_handling_test(){
+    public void window_handling_test() {
 
         // 2. Go to : https://www.amazon.com
         driver.get("https://www.amazon.com");
@@ -37,34 +38,9 @@ public class TC1_WindowHandle {
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
 
-
         // 4. Create a logic to switch to the tab where Etsy.com is open
 
-        Set<String> allWindowsHandles = driver.getWindowHandles();
-
-        for (String each : allWindowsHandles) {
-
-            driver.switchTo().window(each);
-
-            System.out.println("Current URL : " + driver.getCurrentUrl());
-
-            if (driver.getCurrentUrl().contains("etsy")){
-                break;
-            }
-
-        }
-
-
-        // 5. Assert: Title contains “Etsy”
-
-        String actualTitle = driver.getTitle();
-        String expectedInTitle = "Etsy";
-
-        Assert.assertTrue(actualTitle.contains(expectedInTitle));
-
-
-
-
+        BrowserUtils.switchWindowAndVerify(driver, "etsy", "Etsy");
 
     }
 
--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -4,9 +4,14 @@ package com.cydeo.utilities;
 In this class only general utility methods that are not related to some specific page.
  */
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtils {
 
-    public void sleep(int second) {
+    public static void sleep(int second) {
 
         /*
         This method will accept int (in seconds ) and execute Thread.sleep
@@ -20,5 +25,41 @@ public class BrowserUtils {
         }
 
     }
+
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current URL : " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+
+        }
+
+        //  Assert: Title contains “expectedInTitle”
+
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+    }
+
+    /*
+    This method accepts a String "expectedTitle" and Asserts if it true
+    */
+
+    public static void verifyTitle(WebDriver driver, String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+
+    }
+
+
 
 }
